091bf2e3249ac77a80f0d2c18a8b1ee963e6e85f
SangHan <discegaudere24.7@gmail.com>
2022-01-23 21:13:51 +0900

    Feat: add localStorage

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,21 +1,26 @@
+import {useState ,useEffect} from 'react';
 import { Todo } from '../type';
 
-const initialState: Todo[] = []
+const localStorage= window.localStorage.getItem('todos');
+const initialState: Todo[] = localStorage !== null ? JSON.parse(localStorage) : [];
 
 export function reducer(state = initialState, action:any ): any{
   if(action.type === 'plus'){
     let copy = [...state];
     copy.unshift(action.payload);
+    window.localStorage.setItem('todos', JSON.stringify(copy));
     return copy;
   }
   else if(action.type === 'delete'){
     let copy = [...state];
     copy = copy.filter((todo) => todo.id !== action.payload.id)
+    window.localStorage.setItem('todos', JSON.stringify(copy));
     return copy;
   }
   else if(action.type === 'deleteAll'){
     let copy = [...state];
     copy = action.payload.getTodo.filter((todo: Todo) => !todo.checkbox);
+    window.localStorage.setItem('todos', JSON.stringify(copy));
     return copy;
   }
   else if(action.type === 'checked'){
@@ -28,5 +33,4 @@ export function reducer(state = initialState, action:any ): any{
   }
   else return state;
 }
-
 
