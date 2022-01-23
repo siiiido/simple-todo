7f3a59f9d628afd1673be970446d5ebbea1791a0
SangHan <discegaudere24.7@gmail.com>
2022-01-23 20:07:25 +0900

    Feat: add focus todo

--- src/screen/FocusMain.tsx
+++ src/screen/FocusMain.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import {NavLink} from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../App';
+import { Dispatch } from 'redux';
+import { Todo } from '../type';
 
 
 const FocusMain = () => {
+  const getTodo = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch();
+
+  const handleFocusTodo = (focusTodo: Todo) => (e: MouseEvent<HTMLButtonElement>) => {
+    dispatch({ type: 'delete', payload: { id: focusTodo.id, todoText: focusTodo.todoText, checkbox: focusTodo.checkbox }});
+  }
+
   return(
     <div>
       <h1>Focus Mode</h1>
 
       <h4>집중 집중 집중!!</h4>
-      <div>
-        하나의 todo만 적는 곳. (최대 글자 수 몇칸으로??)
-      </div>
-      <div>
-        <button>완료!!</button>
-      </div>
+      {
+        getTodo[0] ?
+        (
+          <>
+            <div> {getTodo[0].todoText}    (최대 글자 수 몇칸으로??)</div>
+            <div>
+              <button onClick={handleFocusTodo(getTodo[0])}>완료!!</button>
+            </div>
+          </>
+        ) :
+        (
+          <h3>열공!!</h3>
+        )
+      }
       <NavLink to='/'>
         <button>todo페이지로 이동</button>
       </NavLink>
