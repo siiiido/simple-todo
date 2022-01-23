17207d4cde9534a891127ca48eba2e9ac5846274
SangHan <discegaudere24.7@gmail.com>
2022-01-23 17:52:20 +0900

    Feat: add redux plus, delete, deleteAll, checked

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,17 +1,32 @@
-import React from 'react';
 import { Todo } from '../type';
-
 
 const initialState: Todo[] = []
 
 export function reducer(state = initialState, action:any ): any{
-  if(action.type === 'add'){
+  if(action.type === 'plus'){
     let copy = [...state];
     copy.unshift(action.payload);
     return copy;
   }
+  else if(action.type === 'delete'){
+    let copy = [...state];
+    copy = copy.filter((todo) => todo.id !== action.payload.id)
+    return copy;
+  }
+  else if(action.type === 'deleteAll'){
+    let copy = [...state];
+    copy = action.payload.getTodo.filter((todo: Todo) => !todo.checkbox);
+    return copy;
+  }
+  else if(action.type === 'checked'){
+    let copy = [...state];
+    copy = copy.map((todo) => {
+      if(todo.id === action.payload.id) todo.checkbox = !todo.checkbox
+      return todo;
+    });
+    return copy;
+  }
   else return state;
 }
-
 
 
--- src/screen/TodoMain.tsx
+++ src/screen/TodoMain.tsx
@@ -1,9 +1,10 @@
-import React, {useState, ChangeEvent, KeyboardEvent} from 'react';
+import React, {useState, ChangeEvent, KeyboardEvent, MouseEvent} from 'react';
 import {NavLink} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../App';
 import { Dispatch } from 'redux';
 import { nanoid } from 'nanoid';
+import { Todo } from '../type';
 
 
 const TodoMain = () => {
@@ -15,16 +16,28 @@ const TodoMain = () => {
 
   const handleTodoEnter = (e: KeyboardEvent<HTMLInputElement>) => {
     if(textField !== '' && e.key === 'Enter'){
-      dispatch({type: 'add', payload: { id: nanoid(), todoText: textField }});
+      dispatch({type: 'plus', payload: { id: nanoid(), todoText: textField, checkbox: false }});
       setTextField('');
     }
   }
 
   const handleTodoClick = () => {
     if(textField !== '') {
-      dispatch({type: 'add', payload: { id: nanoid(), todoText: textField }});
+      dispatch({type: 'plus', payload: { id: nanoid(), todoText: textField, checkbox: false }});
       setTextField('');
     }
+  }
+
+  const handleTodoDeleteAll = () => {
+    dispatch({ type: 'deleteAll', payload: {getTodo}})
+  }
+
+  const handleTodoDelete = (todo: Todo) => (e:  MouseEvent<HTMLButtonElement>) => {
+    dispatch({ type: 'delete', payload: { id: todo.id, todoText: todo.todoText, checkbox: todo.checkbox }});
+  }
+
+  const handleCheck = (todo: Todo) => (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: 'checked', payload: { id: todo.id, todoText: todo.todoText, checkbox: todo.checkbox }});
   }
 
   return (
@@ -33,14 +46,16 @@ const TodoMain = () => {
       <input type='text' value={textField} onChange={handleText} onKeyPress={handleTodoEnter} placeholder='write what you want to do' />
       <button onClick={handleTodoClick}>+버튼</button>
       <div>
-      {getTodo.map((todo) => (
-        <div key={todo.id}>
-          <input type='checkbox'/> { todo.todoText }
-        </div>
-      ))}
+        {getTodo.map((todo) => (
+          <div key={todo.id}>
+            <input type='checkbox' onChange={handleCheck(todo)} checked={todo.checkbox}  /> { todo.todoText }
+            {' '}  
+            <button onClick={handleTodoDelete(todo)}>삭제</button>
+          </div>
+        ))}
       </div>
       <div>
-        <button>체크 표시 모두 삭제</button>
+        <button onClick={handleTodoDeleteAll}  >체크 표시 모두 삭제</button>
       </div>
       <NavLink to='/focus'>
         <button>Focus 페이지로 이동</button>
